85ccd5b7f834d986136f9bf6f63bc119c4ab2f5f
Rinat <ringarifullin@gmail.com>
2015-10-31 20:03:57 +0300

    Update db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151031131209) do
+ActiveRecord::Schema.define(version: 20151031154622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string   "title"
+    t.string   "recurrence"
+    t.text     "description"
+    t.text     "schedule"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "events", ["user_id"], name: "index_events_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "full_name"
@@ -40,4 +52,5 @@ ActiveRecord::Schema.define(version: 20151031131209) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "events", "users"
 end
