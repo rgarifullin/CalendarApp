2d22ad6ba6f7cdb6fa4d0b3fb5040a1d646e1372
Rinat <ringarifullin@gmail.com>
2015-10-31 20:16:45 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  root 'events#index'
+
+  resources :users do
+    resources :events
+    get 'list' => 'events#list'
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
