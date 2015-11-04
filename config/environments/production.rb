cba1013d57041398afdbc84ac1eca26d5d214a54
Rinat <ringarifullin@gmail.com>
2015-11-04 20:31:31 +0300

    Add heroku mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,8 +62,20 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.default_url_options = { :host => 'whispering-everglades-5871.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.yandex.com',
+    port: 465,
+    domain: 'whispering-everglades-5871.herokuapp.com',
+    authentication: 'plain',
+    tls: true,
+    user_name: ENV["MAIL_USERNAME"],
+    password: ENV["MAIL_PASSWORD"]
+  }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
